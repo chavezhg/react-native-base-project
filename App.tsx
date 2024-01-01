1ccfca08a4ccfedb5d52461a5e9436720498da94
Ricardo Mendoza <ramch_01@hotmail.com>
2024-01-01 17:37:01 -0600

    Add project files

--- App.tsx
+++ App.tsx
@@ -1,20 +1,106 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
 
-export default function App() {
+import * as React from 'react';
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+import HomeScreen from './pages/HomeScreen';
+import ExploreScreen from './pages/ExploreScreen';
+import SettingScreen from './pages/SettingScreen';
+
+const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+const Tab = createBottomTabNavigator();
+
+const BottomTabStack = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Tab.Navigator
+      initialRouteName="HomeScreen"
+      screenOptions={{headerShown: false}}>
+      <Tab.Screen
+        name="HomeScreen"
+        component={HomeScreen}
+        options={{
+          tabBarLabel: 'Home Screen',
+          /*tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons
+              name="home"
+              color={color}
+              size={size}
+            />
+          ),*/
+        }}
+      />
+      <Tab.Screen
+        name="ExploreScreen"
+        component={ExploreScreen}
+        options={{
+          tabBarLabel: 'Explore Screen',
+          /*tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons
+              name="settings"
+              color={color}
+              size={size}
+            />
+          ),*/
+        }}
+      />
+    </Tab.Navigator>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const HomeScreenStack = () => {
+  return (
+    <Stack.Navigator
+      initialRouteName="HomeScreen"
+      screenOptions={{headerShown: false}}>
+      <Stack.Screen name="BottomTabStack" component={BottomTabStack} />
+    </Stack.Navigator>
+  );
+};
+
+const SettingScreenStack = () => {
+  return (
+    <Stack.Navigator
+      initialRouteName="SecondPage"
+      screenOptions={{headerShown: false}}>
+      <Stack.Screen name="SettingScreen" component={SettingScreen} />
+    </Stack.Navigator>
+  );
+};
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#f4511e', //Set Header color
+          },
+          headerTintColor: '#fff', //Set Header text color
+        }}>
+        <Drawer.Screen
+          name="HomeScreenStack"
+          options={{
+            drawerLabel: 'Home Screen Option',
+            title: 'Home Screen',
+          }}
+          component={HomeScreenStack}
+        />
+        <Drawer.Screen
+          name="SettingScreenStack"
+          options={{
+            drawerLabel: 'Setting Screen Option',
+            title: 'Setting Screen',
+          }}
+          component={SettingScreenStack}
+        />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
---
+++ pages/ExploreScreen.tsx
@@ -0,0 +1,51 @@
+import {Button, View, Text, SafeAreaView} from 'react-native';
+
+const ExploreScreen = ({navigation}: {navigation: any}) => {
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <View style={{flex: 1, padding: 16}}>
+        <View
+          style={{
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+          <Text
+            style={{
+              fontSize: 25,
+              textAlign: 'center',
+              marginBottom: 16,
+            }}>
+            Explore Screen
+          </Text>
+          <Button
+            onPress={() => navigation.navigate('SettingScreen')}
+            title="Go to Setting Screen"
+          />
+          <Button
+            onPress={() => navigation.navigate('HomeScreen')}
+            title="Go to Home Screen"
+          />
+        </View>
+        <Text
+          style={{
+            fontSize: 18,
+            textAlign: 'center',
+            color: 'grey'
+          }}>
+          React Navigate Drawer with Bottom Tab
+        </Text>
+        <Text
+          style={{
+            fontSize: 16,
+            textAlign: 'center',
+            color: 'grey'
+          }}>
+          Hello World
+        </Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default ExploreScreen;
